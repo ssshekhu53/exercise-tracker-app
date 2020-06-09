80ac118d9dda140f69e73f80ddd70c6f20c37a1b
Shashank Shekhar <ssshekhu53@gmail.com>
2020-06-09 10:16:54 +0530

    Modified

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
-const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
